4316d38df64d354bf7679057e8ecadd20e072109
Nikolai Prigodich <nikolai.prigodich@ubisoft.com>
2023-04-01 19:46:32 +0200

    Setup unit tests

---
+++ tests/test_node.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "datastore/node.hpp"
+
+TEST_CASE("insert_or_assign_child adds a child", "[node]")
+{
+}
---
+++ tests/test_volume.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "datastore/volume.hpp"
+
+TEST_CASE("Volumes are stored to disk", "[volume]")
+{
+}
